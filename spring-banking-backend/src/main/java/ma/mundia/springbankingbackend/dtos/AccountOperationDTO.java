ddd72e8d623022f636f45ed0135707270d8552cd
Mohamed Aassou <mohamedaassou12@gmail.com>
2025-12-30 17:05:04 +0100

    feat(back): add DTO classes for account operations and transactions

---
+++ spring-banking-backend/src/main/java/ma/mundia/springbankingbackend/dtos/AccountOperationDTO.java
@@ -0,0 +1,16 @@
+package ma.mundia.springbankingbackend.dtos;
+
+import lombok.Data;
+import ma.mundia.springbankingbackend.enums.OperationType;
+
+import java.util.Date;
+
+@Data
+public class AccountOperationDTO {
+    private Long id;
+    private Date operationDate;
+    private double amount;
+    private OperationType type;
+    private String description;
+}
+
